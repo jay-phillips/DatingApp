ecaad7c27c43490ee37932802d3ca72e41b32bc9
Jerone Phillips <jeronephillips@outlook.com>
2020-07-21 13:11:14 -0400

    Values API test

--- DatingApp.API/Controllers/ValuesController.cs
+++ DatingApp.API/Controllers/ValuesController.cs
@@ -6,8 +6,11 @@ using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 
 using DatingApp.API.Data;
+using Microsoft.AspNetCore.Authorization;
+
 namespace DatingApp.API.Controllers
 {
+   
     [Route("api/[controller]")]
     [ApiController]
     public class ValuesController : ControllerBase
@@ -19,6 +22,7 @@ namespace DatingApp.API.Controllers
 
         }
         // GET api/values
+        [Authorize]
         [HttpGet]
         public async Task<IActionResult> GetValues()
         {
@@ -28,6 +32,7 @@ namespace DatingApp.API.Controllers
         }
 
         // GET api/values/5
+        [AllowAnonymous]
         [HttpGet("{id}")]
         public async Task<IActionResult> GetValue(int id)
         {
